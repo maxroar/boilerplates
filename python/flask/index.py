d2317ec6e34161709f6bed52b62caa6734118fc6
Max Rohrer <mxrohrer@gmail.com>
2017-01-15 16:36:32 -0800

    fixed a typo

--- python/flask/index.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, redirect, request, session
-app = Flask(__name__)
-app.secret_key = 'key'
-
-@app.route('/')
-def display_index():
-    return render_template('index.html')
-
-app.run(debug=True)
