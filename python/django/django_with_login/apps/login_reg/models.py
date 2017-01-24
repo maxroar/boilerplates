4119940dae1e21f4e888b31bf1a265c07a9d4937
Max Rohrer <mxrohrer@gmail.com>
2017-01-23 19:53:43 -0800

    completed registration form validation

--- python/django/django_with_login/apps/login_reg/models.py
+++ python/django/django_with_login/apps/login_reg/models.py
@@ -4,6 +4,29 @@ from django.db import models
 from django.contrib import messages
 
 # Create your models here.
+class UserManager(models.Manager):
+    def validate_reg(self, postData):
+        # regex patters
+        REGEX_NAME = re.compile(r'^([a-zA-Z]){2,55}$')
+        REGEX_EMAIL = re.compile(r'^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$')
+        REGEX_PASS = re.compile(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$')
+        # list to hold error messages
+        errors = []
+        if not REGEX_NAME.match(postData['first_name']):
+            errors.append('First name must be only letters and at least 2 characters.')
+        if not REGEX_NAME.match(postData['last_name']):
+            errors.append('Last name must be only letters and at least 2 characters.')
+        if not REGEX_EMAIL.match(postData['email']):
+            errors.append('Please use a valid email.')
+        if not REGEX_PASS.match(postData['pass1']):
+            errors.append('Password must be at least 8 characters and contain at least 1 of each: capital letter, lowercase letter, number, special character.')
+        if not postData['pass1'] == postData['pass2']:
+            errors.append('Passwords must match.')
+        return errors
+    # def validate_login(self, postData):
+    #
+
+
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -11,29 +34,4 @@ class User(models.Model):
     password = models.CharField(max_length=55)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-
-class UserManager(models.Manager):
-    REGEX_NAME = re.compile(r'^([a-zA-Z]){2,55}$')
-    REGEX_EMAIL = re.compile(r'^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$')
-    REGEX_PASS = re.compile(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{8,20}$')
-
-    def validate_reg(self, postData):
-        valid_form = True
-        if not REGEX_NAME.match(request.POST['first_name']):
-        messages.add_message(request, messages.ERROR, 'First name must be only letters and at least 2 characters.')
-        valid_form = False
-        if not REGEX_NAME.match(request.POST['last_name']):
-            messages.add_message(request, messages.ERROR, 'Last name must be only letters and at least 2 characters.')
-            valid_form = False
-        if not REGEX_EMAIL.match(request.POST['email']):
-            messages.add_message(request, messages.ERROR, 'Please use a valid email.')
-            valid_form = False
-        if not REGEX_PASS.match(request.POST['pass1']):
-            messages.add_message(request, messages.ERROR, 'Password must be at least 8 characters and contain at least 1 of each: capital letter, lowercase letter, number, special character.')
-            valid_form = False
-        if not request.POST['pass1'] == request.POST['pass2']:
-            messages.add_message(request, messages.ERROR, 'Passwords must match.')
-            valid_form = False
-        return valid_form
-    def validate_login(self, postData):
-        
+    objects = UserManager()
--- python/django/django_with_login/apps/login_reg/views.py
+++ python/django/django_with_login/apps/login_reg/views.py
@@ -8,9 +8,20 @@ def index(request):
         messages.add_message(resuest, messages.ERROR, 'You were already logged in, goof. If you would like to log into or register a new account, please log out first.')
         print('logged in')
         return redirect('/success')
-    return render(request, '/login_reg/index.html')
-def success(request):
-    return render(request, '/login_reg/success.html')
+    return render(request, 'login_reg/index.html')
 def register(request):
-    # blahblahblah
+    errors = User.objects.validate_reg(request.POST)
+    if len(errors) > 0:
+        for error in errors:
+            messages.error(request, error)
+        return redirect('/')
+    
     return redirect('/success')
+def login(request):
+    return redirect('/success')
+
+def success(request):
+    return render(request, 'login_reg/success.html')
+def logout(request):
+    request.session.flush()
+    return redirect('/')
